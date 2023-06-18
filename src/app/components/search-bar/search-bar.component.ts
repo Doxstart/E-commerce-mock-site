8cc802c77dce05db6dd3d2987d771ca25fffa2b5
Doxstart <127298109+Doxstart@users.noreply.github.com>
2023-06-19 01:44:13 +0200

    minor changes to the layout

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
@@ -9,20 +9,53 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatInputModule, MatIconModule, MatSelectModule, MatFormFieldModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatInputModule,
+    MatIconModule,
+    MatSelectModule,
+    MatFormFieldModule,
+  ],
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss']
+  styleUrls: ['./search-bar.component.scss'],
 })
 export class SearchBarComponent implements OnInit {
-  searchQuery: string = "";
+  @Input() searchQuery: string = '';
 
   @Output()
   searchTextChanged: EventEmitter<string> = new EventEmitter<string>();
 
-  ngOnInit(): void { }
+  @Output()
+  filteredResult = new EventEmitter<string[]>();
 
-  onSearchTextChanged(){
+  ngOnInit(): void {}
+
+  onSearchTextChanged() {
     this.searchTextChanged.emit(this.searchQuery);
   }
 
+  // applyFilterBy(searchQuery: string) {
+  //   if (searchQuery === 'starts with') {
+  //     const inputValue = this.searchQuery as any as Array<any>
+  //     const filteredStrings = inputValue.filter((str: string) =>
+  //       str.startsWith(searchQuery)
+  //     );
+  //   } else if (searchQuery === 'ends with') {
+  //     const inputValue = this.searchQuery as any as Array<any>
+  //     const filteredStrings = inputValue.filter((str: string) =>
+  //       str.endsWith(searchQuery)
+  //     );
+  //   } else if (searchQuery === 'equals to') {
+  //     const inputValue = this.searchQuery as any as Array<any>
+  //     const filteredStrings = inputValue.filter((str: string) => str === searchQuery);
+  //   } else if (searchQuery === 'includes') {
+  //     const inputValue = this.searchQuery as any as Array<any>
+  //     const filteredStrings = inputValue.filter((str: string | string[]) =>
+  //       str.includes(searchQuery)
+  //     );
+  //   }
+
+  //   this.applyFilterBy.next(this.filteredResult);
+  // }
 }
